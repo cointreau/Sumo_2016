2f6e24115cbb53449e6d07097d7b0c0613ac831d
Wonkyung Yun <wkyun@se.kaist.ac.kr>
2016-07-12 22:47:21 +0900

    policy2 코드 반영

--- KCC2016/src/MainController.java
+++ KCC2016/src/MainController.java
@@ -105,8 +105,10 @@ public class MainController {
 		}
 
 
-		int flagSoS = -1;			// -1-- normal state, 0-- SoS state before control, else -- SoS state after control
-		
+		int SoSstate = 0;			// 0-- normal state, minus number-- SoS state before control, plus number -- SoS state after control
+		int flagedTime = 0;			// state 가 sos로 바뀐 시점을 감지하여 저장
+		String ambulanceId = "";
+		List<String> ambulanceRoute = null;
 		// #### 시뮬레이션 시작.
 		for (int i=0; i<3600; i++){
 
@@ -121,11 +123,15 @@ public class MainController {
 				conn.do_job_set(Vehicle.add("genr"+i, "car", "genr1", simtime, 0, 13.8, (byte) 0));
 			else if (randNum >= 80 && randNum <90)
 				conn.do_job_set(Vehicle.add("gern"+i, "car", "genr2", simtime, 0, 13.8, (byte) 0));
-			else if (randNum >= 90 && randNum <100)
+			else if (randNum >= 90 && randNum <99)
 				conn.do_job_set(Vehicle.add("genr"+i, "car", "genr3", simtime, 0, 13.8, (byte) 0));
+			else if (randNum >= 99 && randNum <100 && SoSstate==0){
+				conn.do_job_set(Vehicle.add("ambul"+i, "ambulance", "ambul1", simtime, 0, 25.0, (byte) 0));
+				ambulanceRoute = (List<String>)conn.do_job_get(Vehicle.getRoute("ambul"+i));
+			}
 
 			//traffic light 주기적 업데이트
-			if (i%trafficLightUpdateCycle ==0 && flagSoS==-1){		//10초마다
+			if (i%trafficLightUpdateCycle ==0 && SoSstate==0){		//10초마다
 				for (Entry<String, CS> e: csList.entrySet()){
 					if (e.getKey().compareTo("01")==0 || e.getKey().compareTo("04")==0 || e.getKey().compareTo("31")==0 || e.getKey().compareTo("34")==0)
 						continue;
@@ -144,19 +150,61 @@ public class MainController {
 					}
 				}
 			}
+			
+			List<String> vehicles = (List<String>) conn.do_job_get(Vehicle.getIDList());
+			if (SoSstate != 2 && SoSstate != -2){
+				for (String str: vehicles){
+					if (str.startsWith("ambul")){
+						ambulanceId = str;
+						SoSstate = -2;
+					}
+				}
+			}
+			if (SoSstate == -2){
+				flagedTime = i;
+				SoSstate = 2;
+			}
+			
 				
 			//List<String> monitoringEdges = (List<String>) conn.do_job_get(Route.getEdges("rush1"));	
 					/*csList.get("12").getCamera().get("A2S") + csList.get("12").getCamera().get("B2E") +
 					csList.get("23").getCamera().get("B3S") + csList.get("23").getCamera().get("C3S");*/
 			
-			if (nCar >= changeToSoS && flagSoS==-1){		//SoS 상황 설정 -- 해당 차로에 100대가 넘는 차량이 몰려있을 경우를 SoS 상황으로 설정함.
-				flagSoS = 0;
+			//policy2의 적용 -- 기존에 policy1의 발동과 상관 없이 
+			if (SoSstate == 2){
+				//기존의 신호등을 원래대로 돌린 뒤
+				for (Entry<String, CS> e: csList.entrySet()){
+					if (e.getKey().compareTo("01")==0 || e.getKey().compareTo("04")==0 || e.getKey().compareTo("31")==0 || e.getKey().compareTo("34")==0)
+						continue;
+					e.getValue().updateAllTrafficLight(conn, trafficLightSignal);		//기존의 SoSstate==1이었으면 red로 일부 바뀐 신호등이 있기 때문에 이렇게 해주어야함
+					
+					
+					//policy2의 새로운 신호 체계를 적용해야 함. 일단 그 주변 신호는 모두 빨간색으로 업데이트 한뒤
+					if (e.getKey().compareTo("12")==0 || e.getKey().compareTo("13")==0 || e.getKey().compareTo("23")==0)
+						e.getValue().updateAllTrafficLightToRed(conn);
+					for (String tl: ambulanceRoute){
+						for (String key: e.getValue().gettlightMap().keySet()){
+							if (key.startsWith(tl)){		//만약 이 edge 명으로 시작하면,
+								e.getValue().updateTrafficLight(conn, key, policyList.get(1).getOperation().getLight());		//해당 edge에서 빠져나가는 노드는 모두 g로 바꾸어줌.
+							}
+						}
+						conn.do_job_set(Trafficlights.setRedYellowGreenState(e.getKey(), e.getValue().getTLight()));
+					}
+				}
+				System.out.println("Ambulance!!");
+			}
+		
+			
+			//policy1의 적용. 우선순위가 가장 낮으므로 어떠한 상황도 아닌 normal 상황에서만 발동하게 된다
+			if (nCar >= changeToSoS && SoSstate==0){		//SoS 상황 설정 -- 해당 차로에 100대가 넘는 차량이 몰려있을 경우를 SoS 상황으로 설정함.
+				SoSstate = -1;
+				flagedTime = i;
 			}
 
-			System.out.println(simtime+" / "+nCar);
+			System.out.println((simtime/1000)+" / "+nCar);
 
-			//SoS 상황시 대응
-			if (flagSoS == 0){
+			//SoS 상황시 대응 (policy1)
+			if (SoSstate == -1){
 				//모든 신호등을 r로 바꾼 뒤, 
 				System.out.println("SOS!!!!");
 				SumoStringList strList = new SumoStringList(monitoringEdges);
@@ -165,14 +213,14 @@ public class MainController {
 					if (e.getKey().compareTo("01")==0 || e.getKey().compareTo("04")==0 || e.getKey().compareTo("31")==0 || e.getKey().compareTo("34")==0)
 						continue;
 
-					//해당 길목(rush1)에 해당하는 신호등들의 신호만 g로 바꿈.
+					//해당 길목(rush1)에 해당하는 신호등들의 신호만 g로 바꿈. 이부분 코드 수정 필요? rush1에 해당하는 노드를 하드 코딩 말고 뭔가 다른 방법으로 알아내어야 함
 					if (e.getKey().compareTo("02")==0 || e.getKey().compareTo("12")==0 || e.getKey().compareTo("13")==0 || e.getKey().compareTo("23")==0 || e.getKey().compareTo("33")==0){
 						e.getValue().updateAllTrafficLightToRed(conn);
 
 						for (String tl: strList){
 							for (String key: e.getValue().gettlightMap().keySet()){
 								if (key.startsWith(tl)){		//만약 이 edge 명으로 시작하면,
-									e.getValue().updateTrafficLight(conn, key, "g");		//해당 edge에서 빠져나가는 노드는 모두 g로 바꾸어줌.
+									e.getValue().updateTrafficLight(conn, key, policyList.get(1).getOperation().getLight());		//해당 edge에서 빠져나가는 노드는 모두 g로 바꾸어줌.
 								}
 							}
 							conn.do_job_set(Trafficlights.setRedYellowGreenState(e.getKey(), e.getValue().getTLight()));
@@ -180,13 +228,18 @@ public class MainController {
 					}
 				}
 
-				flagSoS = i;
-				//만약 상황이 종료되었으면 flag를 다시 0으로 바꿔줌
+				SoSstate = 1;
 			}
 
 			//최소 15tick 후, flag를 0으로 바꿔줌.
-			if (flagSoS +15 < i && flagSoS != -1 && flagSoS != 0){
-				flagSoS = -1;
+			int policyKeepingTime=0;
+			if (SoSstate > 0){
+				policyKeepingTime = policyList.get(SoSstate-1).getOperation().getSustainTime();
+			}
+			if ((flagedTime +policyKeepingTime < i && SoSstate == 1) || (flagedTime+policyKeepingTime < i && SoSstate == 2)){
+				SoSstate = 0;
+				flagedTime = 0;
+				ambulanceId = "";
 				//tlight 정상화
 				for (Entry<String, CS> e: csList.entrySet()){
 					if (e.getKey().compareTo("01")==0 || e.getKey().compareTo("04")==0 || e.getKey().compareTo("31")==0 || e.getKey().compareTo("34")==0)
